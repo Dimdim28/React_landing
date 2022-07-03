fc3152cf938abf5203bb49e3bf7ace63ef33f2bb
Dimdim28 <wwwdimanes1@gmail.com>
2022-07-03 13:43:02 +0300

    Adding a what is GPT3 section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+
+import React from 'react';
 import './feature.css';
 
-const Feature = () => {
-  return (
-    <div>
-      Feature
+const Feature = ({ title, text }) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
     </div>
-  )
-}
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
 export default Feature;
