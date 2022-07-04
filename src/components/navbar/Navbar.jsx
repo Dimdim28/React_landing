062cf2d2f49436469de14d289a3c5d93e9dee504
Dimdim28 <wwwdimanes1@gmail.com>
2022-07-04 09:43:28 +0300

    Adding a possibility section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} alt='logo'/>
         </div>
         <div className="gpt3__navbar-links_container">
         <Menu/>
